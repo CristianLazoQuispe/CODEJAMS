10942dc42ee4a3b0c390a67fb21870b8dab4db5e
CristianLazoQuispe <mecatronico.lazo@gmail.com>
2021-04-20 21:28:11 -0500

    print list

--- Templates/temp.py
+++ Templates/temp.py
@@ -6,10 +6,18 @@ def inputls():
     return list(map(str,input().split(' ')))
 
 
+def print_list(lista):
+    s=""
+    for i in lista[:-1]:
+        s+=str(i)+" "
+    s+=str(lista[-1])
+
+    return s
+
 
 def solve():
 
-    return 20
+    return [20,40,60]
 
 
 t = inputi()
@@ -18,5 +26,6 @@ t = inputi()
 for i in range(t):
 
     ans = solve()
-    print("Case #"+str(i+1)+':',ans,end='')
+    print("Case #"+str(i+1)+':',print_list(ans),end='')
+    #print("Case #"+str(i+1)+':',ans,end='')
     print('') if i!=(t-1) else None
